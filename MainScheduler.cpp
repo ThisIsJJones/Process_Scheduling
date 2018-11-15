badbc45f256ab9534ecc410aff7e82deb375629d
Jordan Jones <jordanjones@Jordans-MacBook-Pro.local>
2018-11-15 17:09:57 -0600

    add processes to vector

--- MainScheduler.cpp
+++ MainScheduler.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <vector>
 #include <fstream>
 #include <string>
 using namespace std;
@@ -19,30 +20,56 @@ class Process{
 	} 
 };
 
+vector<Process*> createProcesses(){
+    vector<Process*> processes;
 
+    string line;
+    ifstream myfile ("process_input.txt");
+    if (myfile.is_open()){
+        getline(myfile, line); //remove first line
+        while ( getline (myfile, line) ){
+            istringstream iss(line); // make line a stream
+            string pid, burst, arrival, priority, deadline, io;
+            
+            //get process input
+            getline(iss, pid, '\t');
+            getline(iss, burst, '\t');
+            getline(iss, arrival, '\t');
+            getline(iss, priority, '\t');
+            getline(iss, deadline, '\t');
+            getline(iss, io, '\t');
+            
+            //convert to int
+            int pid_ = stoi(pid);
+            int burst_ = stoi(burst);
+            int arrival_ = stoi(arrival);
+            int priority_ = stoi(priority);
+            int deadline_ = stoi(deadline);
+            int io_ = stoi(io);
+            
+            //sanitize input
+            if(pid_ >= 0 &&
+               burst_ >= 0 &&
+               arrival_ >= 0 &&
+               priority_ >= 0 &&
+               deadline_ >= 0 &&
+               io_ >= 0){
+                //create process
+                Process* proc = new Process(pid_, burst_, arrival_, priority_, deadline_, io_);
+                processes.push_back(proc);
+            }
+        }
+        myfile.close();
+    }else{
+        cout << "Unable to open file";
+    }
+    return processes;
+}
 
 int main(){
-
-	string line;
-  	ifstream myfile ("process_input.txt");
-  	if (myfile.is_open()){
-		getline(myfile, line); //remove first line
-    		while ( getline (myfile, line) ){ 
-                istringstream iss(line);
-                string pid, burst, arrival, priority, deadline, io;
-                getline(iss, pid, ' ');
-                getline(iss, burst, ' ');
-                getline(iss, arrival, ' ');
-                getline(iss, priority, ' ');
-                getline(iss, deadline, ' ');
-                getline(iss, io, ' ');
-                Process* proc = new Process(stoi(pid), stoi(burst), stoi(arrival), stoi(priority), stoi(deadline), stoi(io));
-    		}
-    		myfile.close();
-  	}else 
-		cout << "Unable to open file"; 
-
-  return 0;
+    vector<Process*> processes = createProcesses();
+    
+    return 0;
 }
 
 
