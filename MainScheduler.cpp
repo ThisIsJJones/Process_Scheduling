03907b819772f59c29a4fa6c23245b835e23cb23
Jordan Jones <jordanjones@Jordans-MacBook-Pro.local>
2018-11-28 23:31:43 -0600

    working for every clock tick :(

--- MainScheduler.cpp
+++ MainScheduler.cpp
@@ -37,11 +37,12 @@ vector<Process*> createProcesses(){
     vector<Process*> processes;
 
     string line;
-    ifstream myfile ("test_input.txt");
+    ifstream myfile ("process_input.txt");
 //      ifstream myfile ("process_input.txt");
 
     if (myfile.is_open()){
         getline(myfile, line); //remove first line
+        
         while ( getline (myfile, line) ){
             istringstream iss(line); // make line a stream
             string pid, burst, arrival, priority, deadline, io;
@@ -62,6 +63,7 @@ vector<Process*> createProcesses(){
             int deadline_ = stoi(deadline);
             int io_ = stoi(io);
             
+            
             //sanitize input
             if(pid_ >= 0 &&
                burst_ >= 0 &&
@@ -69,10 +71,12 @@ vector<Process*> createProcesses(){
                priority_ >= 0 &&
                deadline_ >= 0 &&
                io_ >= 0){
+                
                 //create process
                 Process* proc = new Process(pid_, burst_, arrival_, priority_, deadline_, io_);
                 processes.push_back(proc);
             }
+            
         }
         myfile.close();
     }else{
@@ -97,14 +101,15 @@ bool containProcesses(vector<Process*> processes, vector<queue<Process*> > vecto
     return (processes.size() > 0) || result;
 }
 
-void age_FCFS(vector<queue<Process*> >& vector_queues, int queue_index, int queue_size, int age_interval){
+void age_FCFS(vector<queue<Process*> >& vector_queues, int queue_index, int queue_size, int age_interval, fstream& fs){
     while(queue_size > 0){// go through the queue to age
-        Process* proc_front = vector_queues[queue_index].front();
+        Process* proc_front = vector_queues[queue_index].front(); //grab the last queue from all the queues
         vector_queues[queue_index].pop();
         proc_front->increaseAge();
         if(proc_front->age >= age_interval){ //age it up to the next queue
             proc_front->age = 0; //restart age
             vector_queues[queue_index-1].push(proc_front); //add to the queue above
+            fs << "Process " << proc_front->pid << " has aged and was promoted to queue " << queue_index-1 << "\n";
         }else{
             vector_queues[queue_index].push(proc_front);
         }
@@ -113,115 +118,109 @@ void age_FCFS(vector<queue<Process*> >& vector_queues, int queue_index, int queu
     }
 }
 
-void increase_allWaitingTimes(vector<queue<Process*> >& vector_queues, Process* current_process){
-    for(int i = 0; i<vector_queues.size(); i++){
-        
-        int queue_size = vector_queues[i].size();
-        while(queue_size > 0){// go through the queues
-            
-            Process* proc_front = vector_queues[i].front();
-            vector_queues[i].pop();
-            
-            if((current_process != NULL && proc_front->pid != current_process->pid) || current_process == NULL ){
-                proc_front->increaseWaitingTime();
-            }
-            
-            vector_queues[i].push(proc_front);
-            queue_size--;
-        }
-    }
-}
 
-
-
-
-int main(int argc, char * argv[]){
-    vector<Process*> processes = createProcesses();
-    sort(processes.begin(), processes.end(), compare); //sort the processes by arrival, last is 0
-    
-    //MFQS
-    
+void MFQS(vector<Process*> processes, fstream& fs){
     //an array of queues
     //with an index pointing to which queue we are currently at
+    int queue_index = 0;
     queue<Process*> queue1;
     queue<Process*> queue2;
     queue<Process*> queue3;
+    queue<Process*> queue4;
+    queue<Process*> queue5;
     
-    int queue_index = 0;
     vector<queue<Process*> > vector_queues;
     vector_queues.push_back(queue1);
     vector_queues.push_back(queue2);
     vector_queues.push_back(queue3);
-
+    vector_queues.push_back(queue4);
+    vector_queues.push_back(queue5);
+    
     Process* cpu_process = NULL;
     
     int MFQS_timer = 0;
-    const int master_time_quantum = 5;
+    const int master_time_quantum = 1;
+    int master_queue_time_quantum = 0;
     int time_quantum = master_time_quantum;
-    int age_interval = 5;
+    int age_interval = 10;
     int master_waitTime = 0;
     int master_completed = 0;
     int master_turnaround = 0;
     bool tqexpired = true;
     
     while( containProcesses(processes, vector_queues) ) {
-        cout << "CLOCK: " << MFQS_timer << "\n";
         //gather new processes when their arrival comes
         while(processes.back() != NULL && processes.back()->arrival == MFQS_timer){
             vector_queues[0].push( processes.back() );
             processes.pop_back();
         }
-        cout << "tqe: " << tqexpired << "\n";
+        
         if(tqexpired || (cpu_process != NULL && cpu_process->burst==0)){//we are ready to grab a new process
-            cout << "here\n";
-
+            
             if(cpu_process != NULL && cpu_process->burst==0){
                 master_completed++;
+                cpu_process->waitingTime = (MFQS_timer - cpu_process->waitingTime) - cpu_process->master_burst;
                 master_turnaround += cpu_process->waitingTime + cpu_process->master_burst;
+                
+                
                 master_waitTime += cpu_process->waitingTime; //when the process dies add its waiting time
+                
                 vector_queues[queue_index].pop(); //remove process we just processed
                 tqexpired = true;
+                
+//                fs << "Process " << cpu_process->pid << " ran from " << MFQS_timer - (master_queue_time_quantum - time_quantum) << " through " << MFQS_timer-1 << " in queue " << queue_index << " and has finished\n";
+                
                 delete cpu_process;
                 cpu_process = NULL; // uh oh
             }
             
             queue_index = 0;
-
+            int ji = 0;
             //loop through the queues and stop at the first queue with a process
             while(queue_index < vector_queues.size() && vector_queues[queue_index].size() == 0){ //find a queue with a process
                 queue_index++;
+                ji++;
+                cout<<ji<<"\n";
             }
-            cout << "chosen index: " << queue_index << " size: " << vector_queues[queue_index].size() <<"\n";
+            //            cout << "chosen index: " << queue_index << " size: " << vector_queues[queue_index].size() <<"\n";
             if(queue_index < vector_queues.size()){// need to check if we actually found a queue with processes or we ran over
                 cpu_process = vector_queues[queue_index].front();//grab the first process from the queue
-                cout << "chosen pid: " << cpu_process->pid << "\n";
+                //                cout << "chosen pid: " << cpu_process->pid << "\n";
+                if(cpu_process->master_burst == cpu_process->burst){
+                    cpu_process->waitingTime = MFQS_timer;
+                }
                 cpu_process->burst--;//decrease burst for process
                 tqexpired = false;
                 time_quantum = pow(2, queue_index) * master_time_quantum; //reset time quantum for new process, for current queue
+                master_queue_time_quantum = time_quantum;
             }
             
         }else{
             cpu_process->burst--;//decrease burst for process
         }
         
-        if(queue_index+1 == vector_queues.size()){ //use aging if we are in the last one, else do time quantum
+        
+        cout << "CLOCK: " << MFQS_timer << "\n";
+        
+        if(queue_index+1 == vector_queues.size()){ //if we are in the last one, dont do time quantum
+            
             //do aging
             queue<Process*> temp_queue;
-
-            //remove the first process
+            
+            //remove the first process, because we shouldn't update this processes age timer
             Process* proc_front = vector_queues[queue_index].front();
             vector_queues[queue_index].pop();
             vector_queues[queue_index].push(proc_front);
             
             int queue_size = vector_queues[queue_index].size()-1; //the size of all the ages to increase
-            age_FCFS(vector_queues, queue_index, queue_size, age_interval);//age everything in the last queue
-            
+            age_FCFS(vector_queues, queue_index, queue_size, age_interval, fs);//age everything in the last queue
         }else if(queue_index < vector_queues.size()){ // all queues before last queue
             //do time quantum
             time_quantum--;//decrease time quantum
             
             int queue_size = vector_queues[vector_queues.size()-1].size();//size of the last queue (FCFS)
-            age_FCFS(vector_queues, vector_queues.size()-1, queue_size, age_interval);//age everything in the last queue
+            
+            age_FCFS(vector_queues, vector_queues.size()-1, queue_size, age_interval, fs);//age everything in the last queue
             
             if(time_quantum == 0){//time quantum has expired
                 tqexpired = true;
@@ -229,24 +228,66 @@ int main(int argc, char * argv[]){
                     Process* proc = cpu_process;
                     vector_queues[queue_index].pop();
                     vector_queues[queue_index+1].push( proc );//add the current process to the queue below
-//                    cpu_process=NULL; //remove process so we can grab a new one
+//                    fs << "Process " << proc->pid << " ran from " << MFQS_timer-master_queue_time_quantum+1 << " through " << MFQS_timer << " in queue " << queue_index << " and has been demoted to queue " << queue_index+1 << "\n";
+                    
                 }else{
                     //let process get deleted on next clock tick
                 }
             }
         }
-        increase_allWaitingTimes(vector_queues, cpu_process);
         MFQS_timer++;//increase clock tick
     }
+    fs << "\nStats:\n";
+    fs << "Total wait time: " << master_waitTime << "\n";
+    fs << "Total completed: " << master_completed << "\n";
+    fs << "Average turn around time: " << master_turnaround/master_completed << "\n";
+    fs << "Average wait time: " << master_waitTime/master_completed << "\n";
+}
+
+
+
+
+int main(int argc, char * argv[]){
+//    vector<Process*> processes = createProcesses();
+//    sort(processes.begin(), processes.end(), compare); //sort the processes by arrival, last is 0
     
-    cout << "\nStats:\n";
-    cout << "Total wait time: " << master_waitTime << "\n";
-    cout << "Total completed: " << master_completed << "\n";
-    cout << "Average turn around time: " << master_turnaround/master_completed << "\n";
-    cout << "Average wait time: " << master_waitTime/master_completed << "\n";
+    
+    string read = "";
+    
+    fstream fs;
+    fs.open("test.txt", std::fstream::in | std::fstream::out  | std::fstream::trunc );
+    while(read != "Exit" && read != "5"){
+        fs << flush; // print everything in the buffer
+        cout << "Enter the number of the scheduling algorithm you would like to use?\n";
+        cout << "\tMFQS: 1\n";
+        cout << "\tRTS: 2\n";
+        cout << "\tWHS: 3\n";
+        cout << "\tCreate process: 4\n";
+        cout << "\tExit: 5\n";
+        cout << "Option: ";
+        cin >> read;
+    
+    
+        
+        if(read == "1"){
+            //MFQS
+            vector<Process*> processes = createProcesses();
+            sort(processes.begin(), processes.end(), compare); //sort the processes by arrival, last is 0
+            
+            MFQS(processes, fs);
+        }
+        
+        cout << "\n";
+        cout << "\n";
+        
+    }
+     fs.close();
 
     return 0;
 }
+
+
+
 
 
 
