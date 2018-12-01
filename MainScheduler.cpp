c0eeb7d028bb5cb0a03979ce1d1ef1f16b8a4b26
Jordan Jones <jordanjones@Jordans-MacBook-Pro.local>
2018-12-01 16:17:30 -0600

    add process creation option

--- MainScheduler.cpp
+++ MainScheduler.cpp
@@ -5,7 +5,6 @@
 #include <math.h>
 #include <string>
 #include <queue>
-#include <algorithm>
 #include "Process.h"
 #include "MFQS.h"
 #include "RTS.h"
@@ -13,18 +12,18 @@ using namespace std;
 
 vector<Process*> createProcesses(){
     vector<Process*> processes;
-
+    
     string line;
-//    ifstream myfile ("test_input.txt");
-      ifstream myfile ("process_input.txt");
-
+    //    ifstream myfile ("test_input.txt");
+    ifstream myfile ("process_input.txt");
+    
     if (myfile.is_open()){
         getline(myfile, line); //remove first line
-
+        
         while ( getline (myfile, line) ){
             istringstream iss(line); // make line a stream
             string pid, burst, arrival, priority, deadline, io;
-
+            
             //get process input
             getline(iss, pid, '\t');
             getline(iss, burst, '\t');
@@ -32,7 +31,7 @@ vector<Process*> createProcesses(){
             getline(iss, priority, '\t');
             getline(iss, deadline, '\t');
             getline(iss, io, '\t');
-
+            
             //convert to int
             int pid_ = stoi(pid);
             int burst_ = stoi(burst);
@@ -40,8 +39,8 @@ vector<Process*> createProcesses(){
             int priority_ = stoi(priority);
             int deadline_ = stoi(deadline);
             int io_ = stoi(io);
-
-
+            
+            
             //sanitize input
             if(pid_ >= 0 &&
                burst_ >= 0 &&
@@ -49,12 +48,12 @@ vector<Process*> createProcesses(){
                priority_ >= 0 &&
                deadline_ >= 0 &&
                io_ >= 0){
-
+                
                 //create process
                 Process* proc = new Process(pid_, burst_, arrival_, priority_, deadline_, io_);
                 processes.push_back(proc);
             }
-
+            
         }
         myfile.close();
     }else{
@@ -69,24 +68,25 @@ bool compare(const Process* l, const Process* r){
 
 inline bool isInteger(const string& s){
     if(s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+'))) return false;
-
+    
     char * p;
     strtol(s.c_str(), &p, 10);
-
+    
     return (*p == 0);
 }
 
 
 int main(int argc, char * argv[]){
-//    vector<Process*> processes = createProcesses();
-//    sort(processes.begin(), processes.end(), compare); //sort the processes by arrival, last is 0
-
-
+    //    vector<Process*> processes = createProcesses();
+    //    sort(processes.begin(), processes.end(), compare); //sort the processes by arrival, last is 0
+    
+    
     string readInOption = "";
     string outputFileName = "output.txt";
-
+    
     fstream fs;
     fs.open(outputFileName, std::fstream::in | std::fstream::out  | std::fstream::trunc );
+    vector<Process*> createdProcesses;
     while(readInOption != "5"){
         fs << flush; // print everything in the buffer
         cout << "Enter the number of the scheduling algorithm you would like to use?\n";
@@ -97,26 +97,29 @@ int main(int argc, char * argv[]){
         cout << "\tExit: 5\n";
         cout << "Option: ";
         cin >> readInOption;
-
+        
         int option = -1;
         if(isInteger(readInOption)){
             option = stoi(readInOption);
         }
-
+        
         vector<Process*> processes;
-
+        
         switch(option){
             case 1:
                 //MFQS
                 processes = createProcesses();
+                processes.insert(processes.end(), createdProcesses.begin(), createdProcesses.end());
                 sort(processes.begin(), processes.end(), compare); //sort the processes by arrival, last is 0
-
+                
                 MFQS(processes, fs);
                 cout << "read output in file: " << outputFileName << "\n";
                 break;
             case 2:
                 //RTS
                 processes = createProcesses();
+                processes.push_back( createdProcesses.back() );
+                //                processes.insert(processes.end(), createdProcesses.begin(), createdProcesses.end());
                 sort(processes.begin(), processes.end(), compare); //sort the processes by arrival, last is 0
                 RTS(processes, fs);
                 cout << "read output in file: " << outputFileName << "\n";
@@ -124,22 +127,56 @@ int main(int argc, char * argv[]){
             case 3:
                 //WHS
                 break;
-            case 4:
+            case 4: {
                 //CREATE PROCESS
-
+                string pidS, burstS, arrivalS, priorityS, deadlineS, ioS;
+                
+                cout << "Enter in pid, burst, arrival, priority, deadline, and io for the process:\n";
+                cout << "PID: ";
+                cin >> pidS;
+                cout << "Burst: ";
+                cin >> burstS;
+                cout << "Arrival: ";
+                cin >> arrivalS;
+                cout << "Priority: ";
+                cin >> priorityS;
+                cout << "Deadline: ";
+                cin >> deadlineS;
+                cout << "I/O: ";
+                cin >> ioS;
+                
+                if(isInteger(pidS) &&
+                   isInteger(burstS) &&
+                   isInteger(arrivalS) &&
+                   isInteger(priorityS) &&
+                   isInteger(deadlineS) &&
+                   isInteger(ioS)){
+                    
+                    //convert to int
+                    int pidI_ = stoi(pidS);
+                    int burstI_ = stoi(burstS);
+                    int arrivalI_ = stoi(arrivalS);
+                    int priorityI_ = stoi(priorityS);
+                    int deadlineI_ = stoi(deadlineS);
+                    int ioI_ = stoi(ioS);
+                    
+                    Process* processToAdd = new Process(pidI_, burstI_, arrivalI_, priorityI_, deadlineI_, ioI_);
+                    createdProcesses.push_back(processToAdd);
+                }
+                break;
+            }
                 break;
             case 5:
                 //exit
                 break;
             default:
-
                 cout << "\nPlease enter an option: 1, 2, 3, 4, or 5";
                 break;
         }
         cout << "\n";
     }
-     fs.close();
-
+    fs.close();
+    
     return 0;
 }
 
