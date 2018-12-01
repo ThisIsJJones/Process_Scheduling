33b12269d0496903fb96df0a9a224a7f8c100546
Jordan Jones <jordanjones@Jordans-MacBook-Pro.local>
2018-12-01 16:17:51 -0600

    Should process removal be a clock tick?

--- RTS.cpp
+++ RTS.cpp
@@ -27,17 +27,17 @@ void RTS(vector<Process*> processes, fstream& fs){
     
     Process current_process;
     
-//    while(processes.back() != NULL && processes.back()->arrival <= RTS_TIMER){
-//        Process* proc = processes.back();
-//        priority_queue.push( *proc );
-//        delete proc;
-//        processes.pop_back();
-//    }
-//
-//    while (priority_queue.size() > 0) {
-//        cout << priority_queue.top().pid << " " << priority_queue.top().priority << " " <<  priority_queue.top().deadline << "\n";
-//        priority_queue.pop();
-//    }
+    //    while(processes.back() != NULL && processes.back()->arrival <= RTS_TIMER){
+    //        Process* proc = processes.back();
+    //        priority_queue.push( *proc );
+    //        delete proc;
+    //        processes.pop_back();
+    //    }
+    //
+    //    while (priority_queue.size() > 0) {
+    //        cout << priority_queue.top().pid << " " << priority_queue.top().priority << " " <<  priority_queue.top().deadline << "\n";
+    //        priority_queue.pop();
+    //    }
     
     while(processes.size() != 0 || priority_queue.size() != 0){
         while(processes.back() != NULL && processes.back()->arrival == RTS_TIMER){
@@ -50,14 +50,14 @@ void RTS(vector<Process*> processes, fstream& fs){
         if(haveProcess){
             if(priority_queue.top() != current_process){//are we still the top process
                 //grab the new process that has come in
-                current_process = priority_queue.top();//grab the lowest priority with earliest deadline
+                current_process = priority_queue.top();//grab earliest deadline
                 executingTime=0;
             }
             priority_queue.pop();//remove
         }else if(priority_queue.size() > 0){
             //grab the top process
             executingTime=0;
-            current_process = priority_queue.top(); //grab the lowest priority with earliest deadline
+            current_process = priority_queue.top(); //grab earliest deadline
             priority_queue.pop();//remove
             haveProcess = true;
         }else{
@@ -65,8 +65,33 @@ void RTS(vector<Process*> processes, fstream& fs){
             haveProcess = false;
         }
         
+//        while(haveProcess && (RTS_TIMER >= current_process.deadline
+//                           || (RTS_TIMER+current_process.burst) > current_process.deadline)){
+//            haveProcess=false;
+//            number_removed++;
+//            fs << "\t" << current_process.pid << " removed due to having a deadline already passed or that will pass before its burst\n";
+//
+//            if(haveProcess){
+//                if(priority_queue.top() != current_process){//are we still the top process
+//                    //grab the new process that has come in
+//                    current_process = priority_queue.top();//grab earliest deadline
+//                    executingTime=0;
+//                }
+//                priority_queue.pop();//remove
+//            }else if(priority_queue.size() > 0){
+//                //grab the top process
+//                executingTime=0;
+//                current_process = priority_queue.top(); //grab earliest deadline
+//                priority_queue.pop();//remove
+//                haveProcess = true;
+//            }else{
+//                //we don't have a process and there is nothing in the queue
+//                haveProcess = false;
+//            }
+//        }
+        
         if(haveProcess){
-             fs << "Executing process " << current_process.pid << " with burst " << current_process.burst << " and deadline " << current_process.deadline << " at clock tick " << RTS_TIMER << "\n";
+            fs << "Executing process " << current_process.pid << " with burst " << current_process.burst << " and deadline " << current_process.deadline << " at clock tick " << RTS_TIMER << "\n";
             executingTime++;
             //we have a process
             if(RTS_TIMER >= current_process.deadline
@@ -81,7 +106,7 @@ void RTS(vector<Process*> processes, fstream& fs){
                 if(current_process.burst == 0){
                     //process has finished
                     master_completed++;
-                     fs << "\tFinished from " << (RTS_TIMER-executingTime)+1 << " through " << RTS_TIMER << "\n";
+                    fs << "\tFinished from " << (RTS_TIMER-executingTime)+1 << " through " << RTS_TIMER << "\n";
                     haveProcess = false;
                 }else{
                     //it didn't finish its burst so add it back to the queue
